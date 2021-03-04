5972f5372fdef861060f0e8bac0b5e6dd66343fb
Ana Ceprnja <aceprnja@iun.edu>
2021-03-03 21:12:51 -0700

    2

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const apiRoute = require('./routes/api');
+require('dotenv/config');
 
-
-
+app.use('/api', apiRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.get('/api', (req, res) => {
-    res.send('Welcome to your api');
-});
+
+
+
+mongoose.connect(process.env.DB_HIDE, { useNewUrlParser: true }, () =>
+    console.log('dbconnect'));
+
 app.listen(3000);
---
+++ models/Api.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const ApiSchema = mongoose.Schema({
+    title: String,
+    description: String,
+
+})
